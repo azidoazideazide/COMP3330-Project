2809ac58bae054857df231b27abdba605aaaa6b8
Quikfixx <lcf5578@gmail.com>
2024-11-12 03:01:40 +0800

    Added Favourite function, changed the dummy backend's ip to ipv4 in api_service.dart, change the structure of list_view_item

--- hku_event_ease/lib/list_view.dart
+++ hku_event_ease/lib/list_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-
 import 'event_search_bar.dart';
 import 'models/list_view_item.dart';
 import 'api_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+
 
 class ListViewPage extends StatefulWidget {
   const ListViewPage({super.key});
@@ -42,8 +45,48 @@ class _ListViewPageState extends State<ListViewPage> {
     super.initState();
     // do the API data fetching at the start of init this page
     _pendingEventItems = apiService.fetchListViewItems();
+    _loadFavorites();
   }
   
+    // Toggle favorite status and store it
+  Future<void> _toggleFavorite(ListViewItem item) async {
+    setState(() {
+      item.isFavorite = !item.isFavorite;
+    });
+    await _saveFavorites();
+  }
+
+  // Save favorite items to SharedPreferences
+  Future<void> _saveFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    final favorites = _pendingEventItems.then((items) =>
+        items.where((item) => item.isFavorite).map((e) => e.toJson()).toList());
+    prefs.setString('favoriteEvents', jsonEncode(await favorites));
+    //debug
+    //print("Saved favorites: ${prefs.getString('favoriteEvents')}");
+  }
+
+  // Load favorite items from SharedPreferences
+  Future<void> _loadFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    final favoriteJson = prefs.getString('favoriteEvents') ?? '[]';
+    final List<dynamic> favoriteList = jsonDecode(favoriteJson);
+    final favoriteItems = favoriteList.map((json) => ListViewItem.fromJson(json)).toList();
+    //debug
+    //print("Loaded favorites: $favoriteItems");
+
+    // Wait for _pendingEventItems to complete and get the list of items
+    final eventItems = await _pendingEventItems;
+
+    setState(() {
+      for (var item in favoriteItems) {
+        // Find the index of the favorite item in the fetched list
+        final index = eventItems.indexWhere((e) => e.eventName == item.eventName);
+        if (index != -1) eventItems[index].isFavorite = true;
+      }
+    });
+  }
+
   // Scroll all the way to the return ListTile part 
   // ListTile part deal with layout of each event item in list view
   // TODO:
@@ -54,8 +97,6 @@ class _ListViewPageState extends State<ListViewPage> {
     return Center(
       child: Column(
         children: <Widget>[
-          // Not sure if the search bar should be separated from the generation of this viewPage
-          // since not sure if there will be difference of its mechanism between listView and gridView 
           EventSearchBar(),
           Expanded(
             child: FutureBuilder<List<ListViewItem>>(
@@ -63,29 +104,28 @@ class _ListViewPageState extends State<ListViewPage> {
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(child: CircularProgressIndicator());
-                }
-                else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
                   return const Center(child: Text('No events found'));
-                }
-                else {
+                } else {
                   final eventItems = snapshot.data!;
                   return ListView.builder(
                     itemCount: eventItems.length,
-
-                    // This is actually a loop
                     itemBuilder: (BuildContext context, int index) {
                       final eventItem = eventItems[index];
-
-                      // Later further finalize the component of a list view item
                       return ListTile(
                         leading: Icon(Icons.event),
                         title: Text(eventItem.eventName),
                         subtitle: Text('${eventItem.tagName} - ${eventItem.startDateTime}'),
-                        
-                        // Later Implement and link to a detailed view
-                        onTap:() {
+                        trailing: IconButton(
+                          icon: Icon(
+                            eventItem.isFavorite ? Icons.favorite : Icons.favorite_border,
+                          ),
+                          color: eventItem.isFavorite ? Colors.red : Colors.grey,
+                          onPressed: () => _toggleFavorite(eventItem),
+                        ),
+                        onTap: () {
                           _showImageInfo(context, "Later Implement");
-                        }
+                        },
                       );
                     }
                   );
--- hku_event_ease/lib/models/list_view_item.dart
+++ hku_event_ease/lib/models/list_view_item.dart
@@ -3,13 +3,26 @@ class ListViewItem {
   final String eventName;
   final String tagName;
   final DateTime startDateTime;
+  bool isFavorite;
 
-  const ListViewItem({
+ListViewItem({
     required this.eventId,
     required this.eventName,
     required this.tagName,
     required this.startDateTime,
+    this.isFavorite = false, // default to not favorite
   });
+
+    // Method to convert the object to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'eventId': eventId,
+      'eventName': eventName,
+      'tagName': tagName,
+      'startDateTime': startDateTime.toIso8601String(),
+      'isFavorite': isFavorite,
+    };
+  }
 
   factory ListViewItem.fromJson(Map<String, dynamic> json) {
     return ListViewItem(
@@ -17,6 +30,7 @@ class ListViewItem {
       eventName: json['eventName'],
       tagName: json['tagName'],
       startDateTime: DateTime.parse(json['startDateTime']),
+      isFavorite: json['isFavorite'] ?? false,
     );
   }
 }
