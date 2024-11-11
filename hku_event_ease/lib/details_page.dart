77c885774ad2803be3888cd6961beca22a5dc598
Hannan Javed <hannansahib@gmail.com>
2024-11-11 18:22:14 +0800

    fetch data from local json

--- hku_event_ease/lib/details_page.dart
+++ hku_event_ease/lib/details_page.dart
@@ -1,31 +1,54 @@
 import 'package:flutter/material.dart';
+import 'services/fetch.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   final int imageId;
 
   DetailsPage({required this.imageId});
 
   @override
-  Widget build(BuildContext context) {
-    // Simulate fetching data
-    final imageInfo = 'Details for Image $imageId';
+  _DetailsPageState createState() => _DetailsPageState();
+}
 
+class _DetailsPageState extends State<DetailsPage> {
+  late Future<Map<String, dynamic>> _imageData;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageData = loadJsonData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Image Details'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/$imageId.jpg'),
-            SizedBox(height: 20),
-            Text(
-              imageInfo,
-              style: TextStyle(fontSize: 24),
-            ),
-          ],
-        ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _imageData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No data found'));
+          } else {
+            final imageInfo = snapshot.data!;
+            return ListView.builder(
+              itemCount: imageInfo.length,
+              itemBuilder: (context, index) {
+                final key = imageInfo.keys.elementAt(index);
+                final value = imageInfo[key];
+                return ListTile(
+                  title: Text('Image $key'),
+                  subtitle: Text(value.toString()),
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
