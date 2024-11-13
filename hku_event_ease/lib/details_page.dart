918764c367901a13250c2aeb91b55a85b9ca7e82
Hannan Javed <hannansahib@gmail.com>
2024-11-14 02:32:41 +0800

    add button for adding event to calendar

--- hku_event_ease/lib/details_page.dart
+++ hku_event_ease/lib/details_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'models/list_view_item.dart';
+import 'package:add_2_calendar/add_2_calendar.dart';
 import 'services/api_service.dart';
+import 'models/detail_view_item.dart';
 
 class DetailsPage extends StatefulWidget {
   final String imageId;
@@ -14,7 +13,7 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
-  late Future<ListViewItem> _imageData;
+  late Future<DetailViewItem> _imageData;
   final ApiService apiService = ApiService();
 
   @override
@@ -23,13 +22,32 @@ class _DetailsPageState extends State<DetailsPage> {
     _imageData = apiService.fetchDetailViewItem(widget.imageId);
   }
 
+  void _addEvent(DetailViewItem eventInfo) {
+    final event = Event(
+      title: eventInfo.eventName,
+      description: '${eventInfo.organizerName}, ${eventInfo.venue}',
+      location: eventInfo.venue,
+      startDate: eventInfo.startDateTime,
+      endDate: DateTime.now().add(Duration(hours: 1)),
+      iosParams: IOSParams(
+        reminder: Duration(hours: 1),
+        url: 'https://www.example.com',
+      ),
+      androidParams: AndroidParams(
+        emailInvites: ['example@example.com'],
+      ),
+    );
+
+    Add2Calendar.addEvent2Cal(event);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Image Details'),
       ),
-      body: FutureBuilder<ListViewItem>(
+      body: FutureBuilder<DetailViewItem>(
         future: _imageData,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -57,8 +75,23 @@ class _DetailsPageState extends State<DetailsPage> {
                   ),
                   SizedBox(height: 10),
                   Text(
-                    'Description: ${imageInfo.tagName} - ${imageInfo.startDateTime}',
+                    'Description: ${imageInfo.organizerName}',
                     style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Venue: ${imageInfo.venue}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Start Time: ${imageInfo.startDateTime}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () => _addEvent(imageInfo),
+                    child: Text('Add Event to Calendar'),
                   ),
                 ],
               ),
